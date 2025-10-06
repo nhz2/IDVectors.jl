4e1e51138af643c1def151397eb81493715bce5d
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-10-07 00:49:20 +0200

    Remove unused _grow_fill_field! function

--- src/utils.jl
+++ src/utils.jl
@@ -1,5 +1,3 @@
-
-
 # Copied from base/array.jl because this is not a public function
 # Modified to use Int64 and to have max to prevent overflow
 # https://github.com/JuliaLang/julia/blob/v1.11.6/base/array.jl#L1042-L1056
@@ -26,18 +24,6 @@ end
     end
     new_size = Int(min(overallocation(n), maxsize))
     new_mem = typeof(old_mem)(undef, new_size)
-    unsafe_copyto!(new_mem, 1, old_mem, 1, length(old_mem))
-    setfield!(s, field, new_mem)
-    return
-end
-
-@inline function _grow_fill_field!(s, n::Int64, x, field::Symbol, maxsize::Int64= typemax(Int64))
-    old_mem = getfield(s, field)
-    if n ≤ length(old_mem)
-        return
-    end
-    new_size = Int(min(overallocation(n), maxsize))
-    new_mem = fill!(typeof(old_mem)(undef, new_size), x)
     unsafe_copyto!(new_mem, 1, old_mem, 1, length(old_mem))
     setfield!(s, field, new_mem)
     return
