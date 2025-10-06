79ed3ad94081c897408ab8da1d99de9dca6ca722
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-10-07 01:27:02 +0200

    Update runtests.jl
    Added a function to assert invariants for Gen structure.

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,43 @@ using Aqua: Aqua
 
 Aqua.test_all(UniqueIDs)
 
+function _assert_invariants_id2idx!(s::Gen)
+    @assert length(s.idx_gens) ≤ typemax(UInt32)
+    @assert s.gens_len ≤ length(s.idx_gens)
+    @assert s.n_active ≤ s.gens_len
+    @assert s.target_queue_length > 0
+    @assert s.target_queue_length < typemax(Int32)
+    @assert Int64(s.target_queue_length) + Int64(s.n_active) ≤ typemax(UInt32)
+    n_inactive = count(isodd ∘ last, s.idx_gens)
+    @assert n_inactive == length(s.idx_gens) - s.n_active
+    # Check the free queue
+    if iszero(n_inactive)
+        @assert iszero(s.free_head)
+        @assert iszero(s.free_tail)
+    end
+    visited = zeros(Bool, length(s.idx_gens))
+    p = Int(s.free_head)
+    n = 0
+    while !iszero(p)
+        @assert !visited[p]
+        visited[p] = true
+        n += 1
+        next_p, gen = s.idx_gens[p]
+        if p ≤ s.gens_len
+            @assert isodd(gen)
+        else
+            @assert gen == typemax(UInt32)
+        end
+        # Unlike GenNoWrap gen is allowed to be typemax(UInt32)
+        if iszero(next_p)
+            @assert s.free_tail == p
+        end
+        p = next_p
+    end
+    @assert n == n_inactive
+    nothing
+end
+
 #=
 Don't add your tests to runtests.jl. Instead, create files named
 
